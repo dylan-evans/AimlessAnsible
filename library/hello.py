8c020db41be824a439af2fc43fee4dc4a2b5d100
Dylan Evans <dylan@dje.me>
2022-07-03 00:58:03 +0000

    trim redundant junk

--- library/hello.py
+++ library/hello.py
@@ -1,21 +1,17 @@
 from ansible.module_utils.basic import AnsibleModule
 
+
 def run_module():
     mod_args = {
         "name": {"type": str, "required": True}
     }
 
-    result = {
-        "changed": False,
-        "original_message": "",
-        "message": "",
-    }
-
     module = AnsibleModule(argument_spec=mod_args, supports_check_mode=False)
 
-    result["original_message"] = module.params["name"]
-    result["message"] = f"Hello, {module.params['name']}!"
-    module.exit_json(**result)
+    module.exit_json(
+        changed=False,
+        message=f"Hello, {module.params['name']}!"
+    )
 
 
 if __name__ == '__main__':
